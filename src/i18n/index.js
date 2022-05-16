7f0db3ed4c4738a7e6087d5f8b6d6de9b58af0a1
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-05-16 21:08:30 +0800

    feat(国际化): 国际化缓存

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import zhLocale from './lang/zh.js'
 import enLocale from './lang/en.js'
+import store from '@/store'
 
 const messages = {
   en: {
@@ -14,15 +15,17 @@ const messages = {
     }
   }
 }
-
-const locale = 'zh'
+// 返回当前 lang
+function getLanguage () {
+  return store && store.getters && store.getters.language
+}
 
 const i18n = createI18n({
   // 使用 Composition API 模式，则需要将其设置为false
   legacy: false,
   // 全局注入 $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
