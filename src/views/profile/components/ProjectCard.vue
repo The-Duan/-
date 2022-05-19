951488ddf915560e4ba7e3b64fef725ae2c536e8
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-05-20 05:47:57 +0800

    feat(个人中心): 头像组件创建

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="">项目介绍</div>
+  <div class="">
+    <pan-thumb
+      :image="$store.getters.userInfo.avatar"
+      :height="'100px'"
+      :width="'100px'"
+      :hoverable="false"
+    >
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </pan-thumb>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import PanThumb from '@/components/PanThumb/PanThumb'
+
 </script>
 
 <style lang="scss" scoped></style>
