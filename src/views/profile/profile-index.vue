70f94c9022960c397caf26ba33f8342a078e4bf9
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-05-21 10:52:59 +0800

    feat(个人中心): 接口国际化的问题

--- src/views/profile/profile-index.vue
+++ src/views/profile/profile-index.vue
@@ -3,14 +3,14 @@
     <el-row>
       <el-col :span="6">
 <!--        项目介绍-->
-        <project-card class="project-card"></project-card>
+        <project-card class="project-card" :features="featureData"></project-card>
       </el-col>
       <el-col :span="18">
         <el-card>
           <el-tabs v-model="activeName">
 <!--            项目功能-->
             <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
-              <feature/>
+              <Feature/>
             </el-tab-pane>
 <!--            项目章节-->
             <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
@@ -33,9 +33,16 @@ import ProjectCard from '@/views/profile/components/ProjectCard'
 import Author from '@/views/profile/components/ProjectAuthor'
 import Chapter from '@/views/profile/components/ProjectChapter'
 import Feature from '@/views/profile/components/ProjectFeature'
+import { feature } from '@/api/user'
+import { watchSwitchLang } from '@/utils/i18n'
 
 const activeName = ref('feature')
-
+const featureData = ref([])
+const getFeatureData = async () => {
+  featureData.value = await feature()
+}
+getFeatureData()
+watchSwitchLang(getFeatureData)
 </script>
 
 <style lang="scss" scoped>
