357406a85671868fd34e6b96bb78763f39671009
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-06-24 13:48:50 +0800

    feat(文章排名): 拖拽表格

--- src/views/article-ranking/ArticleRanking.vue
+++ src/views/article-ranking/ArticleRanking.vue
@@ -1,38 +1,39 @@
 <template>
   <div class="article-ranking-container">
+    <el-card class="header">
+      <div class="dynamic-box">
+        <span class="title">{{ $t('msg.article.dynamicTitle') }}</span>
+        <el-checkbox-group v-model="selectDynamicLabel">
+          <el-checkbox
+            v-for="(item, index) in dynamicData"
+            :label="item.label"
+            :key="index"
+          >{{ item.label }}</el-checkbox
+          >
+        </el-checkbox-group>
+      </div>
+    </el-card>
+
     <el-card>
       <el-table ref="tableRef" :data="tableData" border>
-        <el-table-column
-          :label="$t('msg.article.ranking')"
-          prop="ranking"
-        ></el-table-column>
-        <el-table-column
-          :label="$t('msg.article.title')"
-          prop="title"
-        ></el-table-column>
-        <el-table-column
-          :label="$t('msg.article.author')"
-          prop="author"
-        ></el-table-column>
-        <el-table-column
-          :label="$t('msg.article.publicDate')"
-        >
-          <template #default="{ row }">
-            {{ $filters.relativeTime(row.publicDate) }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          :label="$t('msg.article.desc')"
-          prop="desc"
-        ></el-table-column>
-        <el-table-column :label="$t('msg.article.action')">
-          <el-button type="primary" size="mini" @click="onShowClick(row)">{{
-              $t('msg.article.show')
-            }}</el-button>
-          <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{
-              $t('msg.article.remove')
-            }}</el-button>
-        </el-table-column>
+       <el-table-column
+         v-for="(item, index) in tableColumns"
+         :key="index"
+         :prop="item.prop"
+         :label="item.label"
+       >
+         <template #default="{ row }" v-if="item.prop === 'publicDate'">
+           {{ $filters.relativeTime(row.publicDate) }}
+         </template>
+         <template #default="{ row }" v-else-if="item.prop === 'action'">
+           <el-button type="primary"   @click="onShowClick(row)">{{
+               $t('msg.article.show')
+             }}</el-button>
+           <el-button type="danger" size="small" @click="onRemoveClick(row)">{{
+               $t('msg.article.remove')
+             }}</el-button>
+         </template>
+       </el-table-column>
       </el-table>
 
       <el-pagination
@@ -51,9 +52,11 @@
 </template>
 
 <script setup>
-import { ref, onActivated } from 'vue'
+import { ref, onActivated, onMounted } from 'vue'
 import { getArticleList } from '@/api/article'
 import { watchSwitchLang } from '@/utils/i18n'
+import { dynamicData, selectDynamicLabel, tableColumns } from './dynamic/index.js'
+import { tableRef, initSortable } from './sortable/index.js'
 
 // 数据相关
 const tableData = ref([])
@@ -76,6 +79,11 @@ watchSwitchLang(getListData)
 // 处理数据不重新加载的问题
 onActivated(getListData)
 
+// 表格拖拽相关
+onMounted(() => {
+  initSortable(tableData, getListData)
+})
+
 // size 改变触发
 const handleSizeChange = currentSize => {
   size.value = currentSize
@@ -88,10 +96,10 @@ const handleCurrentChange = currentPage => {
   getListData()
 }
 
-// 点击查看
-const onShowClick = row => {}
-// 点击删除
-const onRemoveClick = row => {}
+// // 点击查看
+// const onShowClick = row => {}
+// // 点击删除
+// const onRemoveClick = row => {}
 </script>
 
 <style lang="scss" scoped>
@@ -118,4 +126,11 @@ const onRemoveClick = row => {}
     text-align: center;
   }
 }
+
+::v-deep .sortable-ghost {
+  opacity: 0.6;
+  color: #fff !important;
+  background: #304156 !important;
+}
+
 </style>
